08b85f2c392b103e359646796e45f011db30cdf9
Markeisha <markeisha94@gmail.com>
2022-02-21 14:17:17 -0600

    finished

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -19,25 +19,14 @@ router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next
 });
 
 router.post("/login", checkUsernameExists, (req, res, next) => {
-  let { username, password } = req.body;
-
-  Users.findBy({ username })
-    .first()
-    .then((user) => {
-      if (user && bcrypt.compareSync(password, user.password)) {
-        req.session.user = user;
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
-      } else {
-        res.status(401).json({
-          message: 'Invalid credentials'
-        });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
+  const { password } = req.body;
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user
+    res.json({ message: `Welcome ${req.user.username}`})
+  } else {
+    next({ status: 401, message: 'Invalid credentials'})
+  }
+  
 });
 
 
